a00873aa3307c526c7b1bec7fa1f88593a0224a5
Alicia <aliciacanosuarez@gmail.com>
2019-01-21 16:16:14 +0100

    Mi primer commit

--- duckduckgoose.py
+++ duckduckgoose.py
@@ -1,8 +1,9 @@
 def duckduckgoose(jugador_goose, posicion_jugador_goose):
     lista_jugadores = list(jugador_goose)
     posicion_jugador_goose = int(posicion_jugador_goose)
-    goose = jugador_goose[posicion_jugador_goose]
+    goose = lista_jugadores[posicion_jugador_goose]
     return (goose, posicion_jugador_goose)
 
-
-print (duckduckgoose (['Alicia', 'Blanca', 'Ines', 'Angelica', 'Pablo'], 1))
+oca = duckduckgoose (['Alicia', 'Blanca', 'Ines', 'Angelica', 'Pablo'], 1)[0]
+pato = duckduckgoose (['Alicia', 'Blanca', 'Ines', 'Angelica', 'Pablo'], 1)[1]
+print ("La oca sera: ", oca, "y su posicion es: ", pato)
